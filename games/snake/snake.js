c190fa23914d28d3ae2b7f06375846f6bd916d7b
DaphneLoustalet <114203018+DaphneLoustalet@users.noreply.github.com>
2024-04-22 21:47:02 -0700

    common Aggie themed styling and redirects

--- games/snake/snake.js
+++
@@ -1,8 +0,0 @@
-/* 
-    Randomly generate Food
-    Move Snake using arrow keys
-    If Snake hits edge of board, or itself, end game
-    If Snake hits food, increment points +20 and level +1
-    End Game - print level and points of finish- offer options (plan in UI)
-    New Level, erase current food and regenerate in different randomized place
-*/
---
+++ games/sudoku/sudoku.js
@@ -0,0 +1,35 @@
+/* Play is clicked */
+
+/* 1. Randomly figure out number of spots to show
+   2. Generate random valid solution
+   3. Refresh page, only showing random spots at selected indices
+*/
+
+/* Player pushes arrow keys or space is clicked */
+
+/* 1. Highlight the space
+   2. If valid integer is pressed (0 - 9), place it in the square
+   3. If all spaces have been filled, change solve button to validate solution
+*/
+
+/* Solve Button is clicked */
+
+/* 1. Refresh board showing valid answers
+ */
+
+/* Validate Solution is clicked */
+
+/* 1. Iterate through game 2d array check if it is same as solution array
+ */
+
+function goToMainMenu() {
+  window.location.href = "../../index.html"; // Redirect to the main menu
+}
+
+function solveClicked() {
+  alert("Solve button clicked"); // Display a message indicating Solve button was clicked
+}
+
+function playClicked() {
+  alert("Play button clicked"); // Display a message indicating Play button was clicked
+}
